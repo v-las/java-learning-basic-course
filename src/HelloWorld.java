2cf0fc1c7e42ddb7df585314c094e7bef30ef98e
Vlas Mastyksh <mastykash.vlas@gmail.com>
2025-08-09 18:20:48 +0600

    Update README.md

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.printf("Hello, world!\n");
+        System.out.println("Hello world!");
+    }
+
+    public int add(int num1, int num2) {
+        int sum = num1 + num2;
+        return sum;
     }
 }
