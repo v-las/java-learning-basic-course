2c2f4ce313fc91cf9fa5eb47d4b63d81122bfc87
Vlas Mastyksh <mastykash.vlas@gmail.com>
2025-08-11 06:02:28 +0600

    Add PrintVariables class

---
+++ src/PrintVariables.java
@@ -0,0 +1,34 @@
+public class PrintVariables {
+    public static void main(String[] args) {
+        byte age = 30;
+        short studentCount = 500;
+        int year = 3000;
+        long worldPopulation = 8000000000L;
+
+        System.out.println("Your age: " + age);
+        System.out.println("Amount of students: " + studentCount);
+        System.out.println("A year in the future: " + year);
+        System.out.println("worldPopulation: " + worldPopulation);
+
+        System.out.println();
+
+        float percentage = 5.51f;
+        double average = 12.49018759120395092873509817230598712093587120938570129857d;
+
+        System.out.println("percentage: " + percentage);
+        System.out.println("average: " + average);
+
+        System.out.println();
+
+        boolean isEmpty = true;
+        boolean isRaining = false;
+        char grade = 'A';
+
+        System.out.println("isEmpty: " + isEmpty);
+        System.out.println("isRaining: " + isRaining);
+
+        System.out.println("grade: " + grade);
+
+
+    }
+}
